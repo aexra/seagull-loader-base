59b8460d97327698d1605335191f3f491ac830e2
Nikita Fomin <defdXs@yandex.ru>
2025-05-21 15:17:49 +0300

    Some work

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,20 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn update() -> Result<(), String> {
+    // Считать источник клиента из локального файла conf.json
+    // Проверить во вложенной папке client наличие файла conf.json
+    // Если файла нет или версия не актуальна выполнть обновление
+    // Если актуальная версия - закрыть приложение и запустить клиент
+
+    Ok(())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![update])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src/routes/+page.svelte
@@ -0,0 +1,93 @@
+<script lang="ts">
+  let running = $state(true);
+
+  const handleStart = async () => {
+    console.log("DOWNLOAD START");
+  };
+  const handleProgress = async (p: any) => {
+    console.log(p);
+  };
+  const handleFinish = async () => {
+    console.log("DOWNLOAD END");
+  };
+
+  $effect(() => {
+    // Проверить наличие обновлений через раст
+    // Если найдены, перевести состояние в running
+
+    document.body.addEventListener("download-started", handleStart);
+    document.body.addEventListener("download-progress", handleProgress);
+    document.body.addEventListener("download-finished", handleFinish);
+
+    return () => {
+      document.body.removeEventListener("download-started", handleStart);
+      document.body.removeEventListener("download-progress", handleProgress);
+      document.body.removeEventListener("download-finished", handleFinish);
+    };
+  });
+</script>
+
+<div class={`wrapper ${running && 'running'}`}>
+  <div class={`img loading`}>
+    <img src="./seagull.png" alt="Seagull" class="seagull">
+  </div>
+  <div class="worker">
+
+  </div>
+</div>
+
+<style>
+  .wrapper {
+    padding: 20px;
+    box-sizing: border-box;
+
+    height: clamp(100vh, 100vh, 100vh);
+    width: clamp(100vw, 100vw, 100vw);
+
+    display: flex;
+    flex-direction: column;
+  }
+
+  .wrapper > .img {
+    display: flex;
+    padding: 20px;
+
+    align-items: center;
+    justify-content: center;
+
+    overflow: hidden;
+  }
+
+  .seagull {
+    object-fit: contain;
+    width: 100%;
+    height: 100%;
+  }
+
+  .wrapper > .loading {
+    padding: 40px;
+  }
+
+  .loading > .seagull {
+    animation: 2s ease 0s infinite running seaload;
+  }
+
+  .running > .img {
+    padding: 60px;
+  }
+  .wrapper > .worker {
+    height: 40px;
+  }
+
+  @keyframes seaload {
+    0% {
+      rotate: 0deg;
+    }
+    60% {
+      rotate: 360deg;
+    }
+    100% {
+      rotate: 360deg;
+    }
+  }
+</style>
